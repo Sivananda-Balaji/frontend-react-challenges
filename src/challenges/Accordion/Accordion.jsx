8ba8dea75fa3405d6c2413d3316c0a604cc6473d
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-12-09 00:27:01 +0530

    add multi-select option for accordion challenge

--- src/challenges/Accordion/Accordion.jsx
+++ src/challenges/Accordion/Accordion.jsx
@@ -4,27 +4,67 @@ import style from "./Accordion.module.css";
 
 const Accordion = () => {
   const [openId, setOpenId] = useState(null);
+  const [isMultiSelect, setIsMultiSelect] = useState(false);
+  const [multipleId, setMultipleId] = useState([]);
 
   const handleClick = (id) => {
-    setOpenId((prev) => (prev === id ? null : id));
+    if (isMultiSelect) {
+      let temp = [...multipleId];
+      if (temp.includes(id)) {
+        temp = temp.filter((item) => item !== id);
+        setMultipleId(temp);
+      } else {
+        temp.push(id);
+        setMultipleId(temp);
+      }
+    } else {
+      setOpenId((prev) => (prev === id ? null : id));
+    }
+  };
+  const handleMultiSelect = () => {
+    setIsMultiSelect((prev) => !prev);
+    setMultipleId([]);
+    setOpenId(null);
   };
 
-  return questions.map((question) => {
-    return (
-      <div>
-        <div className={style["questionSection"]}>
-          <p>{question.title}</p>
-          <p
-            onClick={() => handleClick(question.id)}
-            className={style["toggleSign"]}
-          >
-            {question.id === openId ? "-" : "+"}
-          </p>
-        </div>
-        {question.id === openId && <p>{question.info}</p>}
+  return (
+    <>
+      <div className={style.multiSelectQtn}>
+        <p>Is multiple open accordion allowed?</p>
+        <input
+          type="checkbox"
+          value={isMultiSelect}
+          onClick={handleMultiSelect}
+        />
       </div>
-    );
-  });
+      <div>
+        {questions.map((question) => {
+          return (
+            <div>
+              <div className={style["questionSection"]}>
+                <p>{question.title}</p>
+                <p
+                  onClick={() => handleClick(question.id)}
+                  className={style["toggleSign"]}
+                >
+                  {isMultiSelect
+                    ? multipleId.includes(question.id)
+                      ? "-"
+                      : "+"
+                    : question.id === openId
+                    ? "-"
+                    : "+"}
+                </p>
+              </div>
+              {isMultiSelect
+                ? multipleId.includes(question.id) && <p>{question.info}</p>
+                : question.id === openId && <p>{question.info}</p>}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Accordion;
